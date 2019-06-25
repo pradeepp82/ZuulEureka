d5472b1f65d16ad97cf6681a144a77a9315317b6
pradeep.panwar <pradeep.panwar@innoviti.com>
2019-06-25 16:37:59 +0530

    add extra poc

---
+++ boot-security-jdbc-authentication/boot-security-jdbc-authentication/src/main/java/com/javainuse/SpringBootFormHandingApplication.java
@@ -0,0 +1,14 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SpringBootFormHandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormHandingApplication.class, args);
+	}
+}
